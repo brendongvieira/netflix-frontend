0e9023b0855e4083da3668e1290bebd5b7e7c42f
Brendon Vieira <brendon-vieira@live.com>
2022-05-21 21:11:36 -0300

    First commit

---
+++ src/components/Hero/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Hero = ({ filme }) => {
+  return (
+    <div
+      id="hero"
+      class="container-fluid"
+      style={{
+        backgroundImage: `url(${filme.capa})`,
+      }}
+    >
+      <div class="container">
+        <div class="row" id="hero_infos">
+          <div class="col-6">
+            <img class="logo" src={filme.logo} />
+            <h1 class="text-white">Top 1 de hoje no Brasil</h1>
+            <p class="text-white">{filme.descricao?.substr(0, 190)}...</p>
+            <br />
+            <button class="btn btn-lg btn-custom-white">
+              <span class="mdi mdi-play"></span>Assistir
+            </button>
+            <button class="btn btn-lg btn-custom-translucent">
+              <span class="mdi mdi-information-outline"></span>Mais Informações
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
